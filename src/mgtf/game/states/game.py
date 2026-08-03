c0cbd803414d302488764203fafad2c31bbead9e
not-louis-239 <243234869+not-louis-239@users.noreply.github.com>
2026-08-03 17:31:14 +0800

    wip: Add tile neighbour diagnostics and skip rendering of inner walls

--- src/mgtf/game/states/game.py
+++ src/mgtf/game/states/game.py
@@ -17,7 +17,6 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-import math
 import random
 from typing import TYPE_CHECKING
 
@@ -48,6 +47,9 @@ if TYPE_CHECKING:
     from mgtf.game.game import Game
 
 
+NEIGHBOURS = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+
+
 class GameState(State):
     def __init__(self, game: Game) -> None:
         super().__init__(game)
@@ -69,19 +71,23 @@ class GameState(State):
         pass
 
     def take_input(self, keys: ScancodeWrapper, events: list[Event], dt_s: float) -> None:
-        if keys[Controls.MOVE_UP]:
+        if keys[Controls.MOVE_BACK]:
             self.player.pos.z -= self.player.speed * dt_s
-        if keys[Controls.MOVE_DOWN]:
+
+        if keys[Controls.MOVE_FWD]:
             self.player.pos.z += self.player.speed * dt_s
+
         if keys[Controls.MOVE_LEFT]:
             self.player.pos.x -= self.player.speed * dt_s
+
         if keys[Controls.MOVE_RIGHT]:
             self.player.pos.x += self.player.speed * dt_s
 
         # if the player is touching a wall, push them away from the wall
-        for x in range(max(0, math.floor(self.player.pos.x) - 2), min(DUNGEON_W, math.floor(self.player.pos.x) + 3)):
-            for z in range(max(0, math.floor(self.player.pos.z) - 2), min(DUNGEON_H, math.floor(self.player.pos.z) + 3)):
+        for x in range(max(0, round(self.player.pos.x) - 1), min(DUNGEON_W, round(self.player.pos.x) + 2)):
+            for z in range(max(0, round(self.player.pos.z) - 1), min(DUNGEON_H, round(self.player.pos.z) + 2)):
                 tile = self.current_floor[x, z]
+
                 if (
                     self.game.assets.lore.tile_properties[tile.typ].solid
                     and collides(
@@ -91,27 +97,40 @@ class GameState(State):
                         (1, 1, 1)
                     )
                 ):
-                    print((x, 0, z))
-                    print(self.player.pos)
-                    self.player.pos.move_towards_ip((x, 0, z), -self.player.speed * dt_s)
+                    if not (
+                        self.player.pos.x - self.player.hitbox.w / 2 > x + 0.5
+                        or self.player.pos.x + self.player.hitbox.w / 2 < x - 0.5
+                    ):
+                        ...
+                    if not (
+                        self.player.pos.z - self.player.hitbox.d / 2 > z + 0.5
+                        or self.player.pos.z + self.player.hitbox.d / 2 < z - 0.5
+                    ):
+                        ...
 
     def draw(self, surface: Surface) -> None:
         surface.fill(cols.BG)
         all_entities = [self.player]
 
-        render_left = max(0, math.floor(self.player.pos.x) - RENDER_DISTANCE_X)
-        render_right = min(DUNGEON_W - 1, math.floor(self.player.pos.x) + RENDER_DISTANCE_X)
-        render_back = max(0, math.floor(self.player.pos.z) - RENDER_DISTANCE_Z)
-        render_front = min(DUNGEON_H - 1, math.floor(self.player.pos.z) + RENDER_DISTANCE_Z)
+        render_left = max(0, round(self.player.pos.x) - RENDER_DISTANCE_X)
+        render_right = min(DUNGEON_W - 1, round(self.player.pos.x) + RENDER_DISTANCE_X)
+        render_back = max(0, round(self.player.pos.z) - RENDER_DISTANCE_Z)
+        render_front = min(DUNGEON_H - 1, round(self.player.pos.z) + RENDER_DISTANCE_Z)
 
         for z in range(render_back, render_front + 1):
             # Draw tiles first
             for x in range(render_left, render_right + 1):
                 tile = self.current_floor[x, z]
 
-                tile_left = SCREEN_CENTRE_X + (x - self.player.pos.x - 0.5) * TILE_WIDTH
-                tile_top = SCREEN_CENTRE_Z + (z - self.player.pos.z - 1) * TILE_DEPTH - self.player.pos.y * TILE_HEIGHT
+                # Skip rendering wall tiles that are entirely surrounded by walls
+                if (
+                    tile.typ == TileType.WALL
+                    and all(self.current_floor[x + dx, z + dz].typ == TileType.WALL for dx, dz in NEIGHBOURS)
+                ):
+                    continue
 
+                tile_left = SCREEN_CENTRE_X + (x - self.player.pos.x - 0.5) * TILE_WIDTH
+                tile_top = SCREEN_CENTRE_Z + (z - self.player.pos.z - 0.5) * TILE_DEPTH - TILE_HEIGHT
 
                 if tile.typ == TileType.WALL:
                     image = self.game.assets.images.wall
@@ -130,7 +149,18 @@ class GameState(State):
                 entity_left = SCREEN_CENTRE_X + (entity.pos.x - self.player.pos.x - entity.hitbox.w / 2) * TILE_WIDTH
                 surface.blit(entity.image, (entity_left, entity_top))
 
+                # Show hitboxes
                 if self.game.diagnostics.enabled:
                     pg.draw.rect(surface, cols.DIAG_HITBOX_TOP, (entity_left, entity_top, entity.hitbox.w * TILE_WIDTH, entity.hitbox.d * TILE_DEPTH), width=2)
                     pg.draw.rect(surface, cols.DIAG_HITBOX_BOTTOM, (entity_left, entity_top + TILE_HEIGHT * entity.hitbox.h, entity.hitbox.w * TILE_WIDTH, entity.hitbox.d * TILE_DEPTH), width=2)
                     pg.draw.rect(surface, cols.DIAG_HITBOX_FRONT, (entity_left, entity_top + TILE_DEPTH * entity.hitbox.d, entity.hitbox.w * TILE_WIDTH, entity.hitbox.h * TILE_HEIGHT), width=2)
+
+        # Show diagnostics for the four tiles immediately adjacent to the player
+        if self.game.diagnostics.enabled:
+            for dx, dz in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                nx, nz = round(self.player.pos.x) + dx, round(self.player.pos.z) + dz
+
+                tile_left = SCREEN_CENTRE_X + (nx - self.player.pos.x - 0.5) * TILE_WIDTH
+                tile_top = SCREEN_CENTRE_Z + (nz - self.player.pos.z - 0.5) * TILE_DEPTH - TILE_HEIGHT
+
+                pg.draw.rect(surface, cols.DIAG_TILE_NEIGHBOURS, (tile_left, tile_top, TILE_WIDTH, TILE_HEIGHT), width=2)
